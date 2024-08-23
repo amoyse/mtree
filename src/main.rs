289dc1e014c6555ab3963d37438a6a41166ffbf8
amoyse <securemoyse127@protonmail.com>
2024-08-23 11:50:46 +0100

    max depth functionality woohoo

--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,20 @@ struct Args {
     /// Supply directory to be ignored
     #[arg(short, value_name = "directory", default_value = "-null")]
     ignore: String,
+ 
+    /// Supply a maximum depth to be travelled 
+    #[arg(short, long, value_name = "depth", default_value = "2")]
+    max_depth: i32,
 }
 
 struct Counts {
     dirs: i32,
-    files: i32
+    files: i32,
+    max_depth:i32
 }
 
 
-fn walk_dir(start_dir: &str, prefix: &str, all: bool, only_directories: bool, ignore_dir: &str, counts: &mut Counts) -> io::Result<()> {
+fn walk_dir(start_dir: &str, prefix: &str, all: bool, only_directories: bool, ignore_dir: &str, counts: &mut Counts, depth: i32) -> io::Result<()> {
 
     // Sort list of paths
     let mut paths: Vec<_> = fs::read_dir(start_dir).unwrap().map(|r| r.unwrap()).collect();
@@ -35,6 +40,11 @@ fn walk_dir(start_dir: &str, prefix: &str, all: bool, only_directories: bool, ig
     let mut it = paths.iter().peekable();
 
     while let Some(path) = it.next() {
+
+        // if counts.depth > counts.max_depth {
+        //     continue;
+        // }
+
         if only_directories && !path.path().is_dir() {
             continue;
         }
@@ -58,12 +68,16 @@ fn walk_dir(start_dir: &str, prefix: &str, all: bool, only_directories: bool, ig
         if it.peek().is_none() {
             println!("{}└── {}", prefix, entity);
             if path.path().is_dir() {
-                walk_dir(&format!("{}/{}", start_dir, entity), &format!("{}│   ", prefix), all, only_directories, ignore_dir, counts)?;
+                if depth < counts.max_depth {
+                    walk_dir(&format!("{}/{}", start_dir, entity), &format!("{}│   ", prefix), all, only_directories, ignore_dir, counts, depth + 1)?;
+                }
             }
         } else {
             println!("{}├── {}", prefix, entity);
             if path.path().is_dir() {
-                walk_dir(&format!("{}/{}", start_dir, entity), &format!("{}│   ", prefix), all, only_directories, ignore_dir, counts)?;
+                if depth < counts.max_depth {
+                    walk_dir(&format!("{}/{}", start_dir, entity), &format!("{}│   ", prefix), all, only_directories, ignore_dir, counts, depth + 1)?;
+                }
             }
         }
     }
@@ -76,13 +90,14 @@ fn main() -> io::Result<()> {
     let ignore_dir = args.ignore;
     let all = args.all;
     let only_directories = args.directories;
+    let max_depth = args.max_depth;
 
     let start_path = ".";
 
-    let mut counts = Counts { dirs : 0, files : 0 };
+    let mut counts = Counts { dirs : 0, files : 0, max_depth};
 
     println!(".");
-    walk_dir(start_path, "", all, only_directories, &ignore_dir, &mut counts)?;
+    walk_dir(start_path, "", all, only_directories, &ignore_dir, &mut counts, 0)?;
     println!("\n{} directories, {} files", counts.dirs, counts.files);
     Ok(())
 }
